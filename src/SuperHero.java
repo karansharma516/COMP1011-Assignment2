dc495d00e8b98660f64bcc77970b8374a26ce8ee
karan sharma <karan.sharma516@yahoo.com>
2015-02-05 19:01:14 -0800

    added generateRandomPowers method in the superHero class

--- src/SuperHero.java
+++ src/SuperHero.java
@@ -24,4 +24,38 @@ public class SuperHero extends Hero {
 		generateRandomPowers();
 	}
 
+	// PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	/**
+	 * This method randomly generates 3 random super powers from the six powers
+	 * and adds them to the superPower array and remove that power from the arrayList
+	 */
+	private void generateRandomPowers(){
+		
+	// creating String ArrayList of name powerList and adding super powers of the hero in the array list
+        ArrayList<String> powerList = new ArrayList<String>();
+		powerList.add("Super Speed");
+		powerList.add("Super Strength");
+		powerList.add("Body Armour");
+		powerList.add("Flight");
+		powerList.add("Fire Generation");
+		powerList.add("Weather Control");
+		
+	// generating random number 
+		Random rand = new Random();
+	
+		// creating for loop three times to pick random powers and remove the selected power each time
+		for(int i=0; i<3 ; i++){
+		
+		// pick the random power from the arraylist
+		int index = rand.nextInt(powerList.size());
+	
+		// set the value of selected power in the array
+		this.superPower[i] = powerList.get(index);
+		
+		// removing selected power from the arraylist
+		 powerList.remove(index);
+		}
+	}
+	
+	
 }
